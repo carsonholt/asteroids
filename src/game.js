f3a9d4ee1bef4ddf3d187a5634a1e8183373fc5f
carsonholt <cdh1522@gmail.com>
2018-10-09 23:50:30 -0500

    final

--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@ import * as Vector from './vector';
 //import Player from './player';
 
 // screen dimensions
-const WIDTH = 720
+const WIDTH = 900
 const HEIGHT = 600
 
 // Create the canvas and context
@@ -39,15 +39,15 @@ var level = 0;
 var numAst = 5; // number of asteroids to start level
 
 // Velocities and positions for each direction
-var x = WIDTH/2;
-var y = HEIGHT/2;
+var x = 360;
+var y = 300;
 
 var dir = {x: 0, y:0}; // direction vector
 var vel = {x: 0, y:0}; // velocity vector
-var speed = 0.01;
+var speed = 0.1;
 const MAX_SPEED = 2;
 // images
-var player = new Player(WIDTH/2, HEIGHT/2);
+var player = new Player(360, 300);
 player.x = x;
 player.y = y;
 //player.load('img/ship.jpg');
@@ -57,6 +57,11 @@ var asteroidImg;
 // arrays
 var lasers = [];
 var asteroids = [];
+
+// sounds
+var astSnd = new Audio("snd/Hit_Hurt2.wav");
+var shipSnd = new Audio("snd/Hit_Hurt.wav");
+var shootSnd = new Audio("snd/Laser_Shoot4.wav");
 
 /** @function handleKeydown
   * Event handler for keydown events
@@ -80,6 +85,9 @@ function handleKeydown(event) {
 	  break;
 	case 'ArrowDown':
 	  currentInput.down = true;
+	  break;
+	case 'w':
+	  currentInput.w = true;
 	  break;
   }
   event.preventDefault();
@@ -109,6 +117,9 @@ function handleKeyup(event) {
 	case 'ArrowDown':
 	  currentInput.down = false;
 	  break;
+	case 'w':
+	  currentInput.w = false;
+	  break;
   }
 }
 // Attach keyup event handler to the window
@@ -119,15 +130,15 @@ window.addEventListener('keyup', handleKeyup);
   * takes an object and wraps to other side of screen if necessary
   */
 function wrap(obj) {
-	if (obj.x >= WIDTH) {		
-		obj.x %= WIDTH;
+	if (obj.x >= 600) {		
+		obj.x %= 600;
 	} else if (obj.x < 0) {
-		obj.x += WIDTH;
+		obj.x += 600;
 	}
 	if (obj.y >= HEIGHT) {
-		obj.y %= HEIGHT;
+		obj.y %= (HEIGHT);
 	} else if (obj.y < 0){
-		obj.y += HEIGHT;
+		obj.y += (HEIGHT);
 	}
 }
 
@@ -149,62 +160,107 @@ function detectCollision(a, b) {
 // render a number of asteroids
 function startLevel() {
 	level++;
-
+	numAst += 2;
+	
 	while (asteroids.length < numAst) {
 		console.log("asteroid length: " + asteroids.length);
 		var randX = Math.random() * WIDTH;
-		var randY = Math.random() * HEIGHT;
-		if ((randX < (WIDTH/2 - 50) || randX > (WIDTH/2 + 50)) || (randY < HEIGHT/2 - 50 || randY > HEIGHT/2 + 50)) {
-			var randVx = Math.random() * 2 - 1;
-			var randVy = Math.random() * 2 - 1;
+		var randY = Math.random() * (HEIGHT);
+		if ((randX < (310) || randX > (410)) || (randY < HEIGHT/2 - 50 || randY > HEIGHT/2)) {
+			var randVx = Math.random() * 0.5 - 0.25;
+			var randVy = Math.random() * 0.5 - 0.25;
 			var randM = Math.random() * 40 + 20;
 			asteroids.push(new Asteroid(randX, randY, {x:randVx, y:randVy}, randM));
 			console.log("New asteroid at: (" + randX + ", " + randY + ") with speed (" + randVx + ", " + randVy + ") and mass " + randM);
-			//i++;
 		}
 	}
+}
+
+/** displayInfo
+  *	draws each life, displays current level, displays score
+  * param context - the context being drawn on
+  */
+function displayInfo() {
+	ctx.fillStyle = '#FFFFFF';
+	ctx.font = '16px Times New Roman';
+	for (var i = 0; i < lives; i++) {
+		ctx.drawImage(player.ship, 800 + (i*20), 90, 15, 20);
+		ctx.fillText("Lives: ", 750, 100);
+	}
+	ctx.fillText("Level: " + level, 750, 130);
+	ctx.fillText("Score: " + score, 750, 160);
+	ctx.fillText("Controls", 750, 240);
+	ctx.fillText("Arrow keys to move", 750, 270);
+	ctx.fillText("Space to shoot", 750, 300);
+	ctx.fillText("W to warp", 750, 330);
+	return;
 }
 
 function render(elapsedTime) {
 	ctx.clearRect(0, 0, WIDTH, HEIGHT);
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0,0,WIDTH,HEIGHT);
+	ctx.strokeStyle = "#FFFFFF";
+	ctx.beginPath();
+	ctx.moveTo(720, 0);
+	ctx.lineTo(720, HEIGHT);
+	ctx.stroke();
+	displayInfo();
 	player.render(ctx);
 	//player.rotate(ctx);
 	asteroids.forEach(function(asteroid) {
 		//console.log("Asteroid: " + asteroid);
 		asteroid.render(ctx);
 	});
+	lasers.forEach(function(laser) {
+		laser.render(ctx);
+	});
 }
 
-/**
- *
+/** update game state
  */
-function update(elapsedTime) {
-	
+function update(elapsedTime) {	
 	Vector.normalize(vel);
 	asteroids.forEach(function(asteroid) {
 		asteroid.update(elapsedTime);
-		/*if (detectCollision(asteroid, player)) {
+		if (detectCollision(player, asteroid)) {
 			lives--;
-			player.x = WIDTH/2;
+			player.x = 360;
 			player.y = HEIGHT/2;
+			shipSnd.play();
 			return;
-		}*/
-		if ((asteroid.x < 10 || asteroid.x > WIDTH) || (asteroid.y < 1 || asteroid.y > HEIGHT)) {
-			wrap(asteroid);
 		}
 	});
 	
-	lasers.forEach(function(laser) {
+	lasers.forEach(function(laser, index) {
 		laser.update(elapsedTime);
-		if ((laser.x < 0 || laser.x >= WIDTH) || (laser.y < 0 || laser.y >= HEIGHT)) {
+		if ((laser.x < 0 || laser.x >= 720) || (laser.y < 0 || laser.y >= HEIGHT)) {
 			lasers.splice(index,1);
 		}
-		asteroids.forEach(function(asteroid) {
+		asteroids.forEach(function(asteroid, jndex) {
+			console.log("asteroid height: " + asteroid.height);
 			if (detectCollision(laser, asteroid)) {
-				asteroid.split();
-				//delete asteroid.x;
+				console.log("asteroid hit");
+				if (asteroid.m > 25) {
+					asteroid.split(laser);
+					score += 10;
+				} else {
+					score += Math.round(asteroid.m);
+				}
+				asteroids.splice(jndex, 1);
+				lasers.splice(index, 1);
+			}
+			for (var i = 0; i < asteroids.length - i; i++) {
+				if (Math.abs(asteroid.x - asteroids[i].x) < 100 && Math.abs(asteroid.y - asteroids[i].y) < 100) {				
+					if (detectCollision(asteroid, asteroids[i])  && asteroid != asteroids[i]) {
+						asteroid.split(laser);
+						asteroids[i].split(laser);
+						asteroids.splice(jndex, 1);
+						asteroids.splice(i, 1);
+						lasers.splice(index, 1);
+						astSnd.play();
+					}
+				}
 			}
 		});
 	});
@@ -259,10 +315,12 @@ class Player {
 
 	update(deltaT, currentInput, priorInput) {
 		var facing = Vector.rotate({x:0, y:-1}, this.angle);
+		//console.log("Facing x: " + facing.x);
 		if (currentInput.space && !priorInput.space) {
 			// fire laser
-			lasers.push(new Laser(x+15, y, 10));
+			lasers.push(new Laser(this.x, this.y, {x:facing.x, y:facing.y}));
 			console.log("laser fired");
+			shootSnd.play();
 		}
 		if (currentInput.up) {
 			this.velocity = Vector.add(this.velocity, facing);
@@ -287,6 +345,11 @@ class Player {
 		if (currentInput.right) {
 			this.angle += 1;
 			//console.log(x);		
+		}
+		if (currentInput.w && !priorInput.w) {
+			this.x = Math.random() * WIDTH;
+			this.y = Math.random() * (HEIGHT);
+			return;
 		}
 		var temp = Vector.add(this, this.velocity);
 		this.x = temp.x;
@@ -320,6 +383,8 @@ class Asteroid {
 		this.m = m;
 		this.asteroidImg = new Image();
 		this.asteroidImg.src = "img/asteroid.jpg";
+		this.width = m;
+		this.height = m;
 	}
 	
 	render(context) {
@@ -329,6 +394,12 @@ class Asteroid {
 	update(deltaT) {
 		this.x += this.v.x * deltaT;
 		this.y += this.v.y * deltaT;
+		wrap(this);
+	}
+	
+	split(laser) {
+		asteroids.push(new Asteroid(this.x, this.y, Vector.perpendicular(this.v), this.m/Math.sqrt(2)));
+		asteroids.push(new Asteroid(this.x, this.y, Vector.invert(Vector.perpendicular(this.v)), this.m/Math.sqrt(2)));
 	}
 }
 /*function Asteroid(x, y, v, m) {
@@ -350,22 +421,24 @@ Asteroid.prototype.split = function() {
 }*/
 
 // Laser class
-function Laser(x, y, l) {
+function Laser(x, y, v) {
 	this.x = x;
 	this.y = y;
-	this.l = l;
+	this.v = {x:player.velocity.x, y:player.velocity.y};
+	console.log("Laser created at: (" + this.x + ", " + this.y +")");
+	console.log(lasers.length);
 }
 
 Laser.prototype.update = function(deltaT) {
-	this.x += 0.2 * dir;
-	this.y += 0.2 * dir;
+	this.x += deltaT * this.v.x;
+	this.y += deltaT * this.v.y;
 }
 
-Laser.prototype.render = function(deltaT) {
+Laser.prototype.render = function(context) {
+	//context.beginPath();
+	context.strokeStyle = "#FF0000";
 	context.beginPath();
-	context.fillStyle = "#FFFFFF";
-	context.beginPath();
-	context.moveTo(x, y);
-	context.lineTo(x+l, y+l);
+	context.moveTo(this.x, this.y);
+	context.lineTo(this.x+(10*Math.sin(player.angle)), this.y+(10*Math.cos(player.angle)));
 	context.stroke();
 }
